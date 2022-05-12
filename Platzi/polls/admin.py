dd8e4f37c09e39e0b98be4b3fe158bc8f1b46d44
jaimeandresalas <jsalasmoreira@gmail.com>
2022-05-12 17:25:18 -0500

    Se agrega url dinamica

--- Platzi/polls/admin.py
+++ Platzi/polls/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from .models import Question, Choice
 # Registrando modelos en el admin
-from .models import Question
-admin.site.register(Question)
+"""from .models import Question
+admin.site.register(Question)"""
+
+
+class ChoicesInline(admin.StackedInline):
+    model = Choice
+    can_delete = True
+    verbose_name_plural = 'choices'
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = (ChoicesInline,)
--- Platzi/polls/views.py
+++ Platzi/polls/views.py
@@ -5,4 +5,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Estas en la pagina Principal")
+
+
+def detail(request, question_id=None):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id=None):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id=None):
+    return HttpResponse(f"Estas viendo los votos de la pregunta numero {question_id}")
